e7ff8471d5716f1bf0716a0492e34470173bd929
l33th <l33th@localhost.localdomain>
2022-12-03 06:36:38 +0200

    chore: Finish header

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WMouton Portfolio</title>
+        <meta name="description" content="WMouton &bull; Sofware Engineer" />
+      </Head>
+
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+    </div>
+  );
+}
